66c00d2a1faf77a2b7de4add5fe636c0a32dffbf
arxaqapi <arxaqapi@protonmail.com>
2022-12-02 00:02:44 +0100

    fix: install.sh procedure

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 bin_name="ojo"
 install_path=".local/bin"
 
-# cargo build --release
+dune build
 
 if [ -f ~/${install_path}/${bin_name} ]; then
     echo "ojo already exists in" ${install_path}
-    echo "the existing file will be overwritten"
-    rm ~/${install_path}/${bin_name}
+    echo "the existing file will not be overwritten"
+    exit 1
 fi
 
-cp target/release/${bin_name} ~/${install_path}
+cp bin/${bin_name} ~/${install_path}
